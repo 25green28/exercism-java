9019fd8246f21ca4d92af65b5c35bf12cd52482b
Mateusz Laski <25green28@gmail.com>
2026-03-20 19:46:58 +0100

    Update 'Saddle Points' exercise based on mentor feedback

--- src/saddle/points/Matrix.java
+++ src/saddle/points/Matrix.java
@@ -1,83 +1,38 @@
 package saddle.points;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
-record Point(int value, int row, int column){}
+import static java.util.stream.IntStream.range;
 
 class Matrix {
-    private final List<List<Integer>> values;
+    private final int rows;
+    private final int columns;
+    private final int[] rowsMax;
+    private final int[] columnsMin;
 
     Matrix(List<List<Integer>> values) {
-        this.values = values;
+        rows = values.size();
+        columns = rows > 0 ? values.getFirst().size() : 0;
+
+        rowsMax = values.stream().mapToInt(
+                row -> row.stream()
+                        .mapToInt(Integer::intValue)
+                        .max().orElseThrow()
+        ).toArray();
+
+        columnsMin = range(0, columns).map(
+                col -> range(0, rows)
+                        .map(i -> values.get(i).get(col))
+                        .min().orElseThrow()
+        ).toArray();
     }
 
     Set<MatrixCoordinate> getSaddlePoints() {
-        List<Point> maxInRows = new ArrayList<>();
-        List<Point> minInColumns = new ArrayList<>();
-
-        Set<MatrixCoordinate> saddlePoints = new HashSet<>();
-
-        if (!values.isEmpty()) {
-            for (int rowIndex = 0; rowIndex < values.size(); rowIndex++) {
-                maxInRows.addAll(getPoints(rowIndex, values.get(rowIndex).size(), true));
-            }
-        }
-
-        if (!values.isEmpty()) {
-            for (int columnIndex = 0; columnIndex < values.getFirst().size(); columnIndex++) {
-                minInColumns.addAll(getPoints(columnIndex, values.size(), false));
-            }
-        }
-
-        for (Point minInColumn : minInColumns) {
-            for (Point maxInRow : maxInRows) {
-                if (maxInRow.column() == minInColumn.column() &&
-                        maxInRow.row() == minInColumn.row() &&
-                        maxInRow.value() == minInColumn.value()
-                ) {
-                    saddlePoints.add(new MatrixCoordinate(maxInRow.column() + 1, maxInRow.row() + 1));
-                }
-            }
-        }
-
-        return saddlePoints;
-    }
-
-    private List<Point> getPoints(int index, int loopLimit, boolean addIfSmaller) {
-        List<Point> points = new ArrayList<>();
-
-        for (int i = 0; i < loopLimit; i++) {
-            int value = addIfSmaller ? values.get(index).get(i) : values.get(i).get(index);
-            int row = addIfSmaller ? i : index;
-            int column = addIfSmaller ? index : i;
-
-            if (points.isEmpty()) {
-                points.add(new Point(value, row, column));
-                continue;
-            }
-
-            if (points.getFirst().value() == value) {
-                points.add(new Point(value, row, column));
-            }
-
-            boolean condition = addIfSmaller ? points.getFirst().value() < value : points.getFirst().value() > value;
-
-            if (condition) {
-                points.clear();
-                points.add(new Point(value, row, column));
-            }
-        }
-
-        return points;
-    }
-
-    public static void main(String[] args) {
-        Matrix matrix = new Matrix(Arrays.asList(
-                Arrays.asList(9, 8, 7),
-                Arrays.asList(5, 3, 2),
-                Arrays.asList(6, 6, 7)
-        ));
-
-        System.out.println(matrix.getSaddlePoints());
+        return range(0, rows).boxed()
+                .flatMap(row -> range(0, columns)
+                        .filter(columns -> rowsMax[row] == columnsMin[columns])
+                        .mapToObj(col -> new MatrixCoordinate(row + 1, col + 1)))
+                .collect(Collectors.toSet());
     }
 }
